91f97334dd91b390214d817b5dd6a46fdbd5ba13
Dinesh Bhat <drbhat@gmail.com>
2020-03-09 15:48:30 -0400

    added post

--- src/main/java/com/example/docker/HelloDockerController.java
+++ src/main/java/com/example/docker/HelloDockerController.java
@@ -1,13 +1,14 @@
 package com.example.docker;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController("/test")
 public class HelloDockerController {
 
 	
-	@RequestMapping(path = "/hello")
+	@RequestMapping(path = "/hello", method = RequestMethod.GET)
 	public String helloGradle() {
 		return "Hello Docker!";
 	}
--- src/main/java/com/example/docker/WebPageController.java
+++ src/main/java/com/example/docker/WebPageController.java
@@ -2,8 +2,7 @@ package com.example.docker;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.SessionAttributes;
 
 @Controller
@@ -11,7 +10,7 @@ import org.springframework.web.bind.annotation.SessionAttributes;
 public class WebPageController {
 
 	
-	@RequestMapping(value="/welcome", method = RequestMethod.GET)
+	@GetMapping(value="/welcome")
     public String showWelcomePage(ModelMap model){
 		model.put("name", "Brownie");
         return "welcome";
